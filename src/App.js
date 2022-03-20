f345b7487a40d6daa1606a630d48650aed87095f
Vishnu-Aithal <90306959+Vishnu-Aithal@users.noreply.github.com>
2022-03-20 14:37:28 +0530

    view - components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { Card } from "./components/Card";
 import { TopNav } from "./components/TopNav";
 import { FilterOptions } from "./components/FilterOptions";
 import { Hero } from "./components/Hero";
+import { HighlightCategory } from "./components/HighlightCategory";
+import { CartInfo } from "./components/CartInfo";
+import { SignInForm } from "./components/SignInForm";
+import { SignUpForm } from "./components/SignUpForm";
 
 function App() {
     return (
@@ -12,6 +16,13 @@ function App() {
             <TopNav />
             <Card type="home-page" badge="70% off" />
             <Card type="listing" inWishlist={false} />
+            <Card type="listing" inCart={true} />
+            <Card type="cart" />
+            <Card type="wishlist" />
+            <HighlightCategory />
+            <CartInfo />
+            <SignInForm />
+            <SignUpForm />
         </div>
     );
 }
