4766fda4d694730a8b195ee773833c23be5b9f24
Vishnu-Aithal <90306959+Vishnu-Aithal@users.noreply.github.com>
2022-03-20 13:04:52 +0530

    view - components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import { Card } from "./components/Card";
+import { TopNav } from "./components/TopNav";
+import { FilterOptions } from "./components/FilterOptions";
+import { Hero } from "./components/Hero";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <Hero />
+            <FilterOptions />
+            <TopNav />
+            <Card type="home-page" badge="70% off" />
+            <Card type="listing" inWishlist={false} />
+        </div>
+    );
 }
 
 export default App;
