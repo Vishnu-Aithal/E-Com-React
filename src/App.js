1f268e4fd2381ae71263c6f72b57e80b88815498
Vishnu-Aithal <90306959+Vishnu-Aithal@users.noreply.github.com>
2022-03-22 02:36:15 +0530

    Add - Pages And Routes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-import "./App.css";
-import { Card } from "./components/Card";
+import { HomePage } from "./routes/HomePage";
 import { TopNav } from "./components/TopNav";
-import { FilterOptions } from "./components/FilterOptions";
-import { Hero } from "./components/Hero";
-import { HighlightCategory } from "./components/HighlightCategory";
-import { CartInfo } from "./components/CartInfo";
-import { SignInForm } from "./components/SignInForm";
-import { SignUpForm } from "./components/SignUpForm";
+import { Outlet } from "react-router-dom";
+import "./index.css";
 
 function App() {
     return (
         <div className="App">
-            <Hero />
-            <FilterOptions />
-            <TopNav />
-            <Card type="home-page" badge="70% off" />
-            <Card type="listing" inWishlist={false} />
-            <Card type="listing" inCart={true} />
-            <Card type="cart" />
-            <Card type="wishlist" />
-            <HighlightCategory />
-            <CartInfo />
-            <SignInForm />
-            <SignUpForm />
+            <Outlet />
         </div>
     );
 }
