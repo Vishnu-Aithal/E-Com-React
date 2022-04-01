56b2211251f595f9dd8da28b2dbaf10fe465775a
Vishnu-Aithal <90306959+Vishnu-Aithal@users.noreply.github.com>
2022-04-01 13:46:38 +0530

    destructure response and remove console logs

--- src/auth-functions/authHandler.js
+++ src/auth-functions/authHandler.js
@@ -2,14 +2,16 @@ import axios from "axios";
 
 export const signInHandler = async (email, password) => {
     try {
-        const response = await axios.post("/api/auth/login", {
+        const {
+            status,
+            data: { encodedToken, foundUser },
+        } = await axios.post("/api/auth/login", {
             email: email,
             password: password,
         });
-        console.log(response);
-        if (response.status === 200) {
-            const token = response.data.encodedToken;
-            const userId = response.data.foundUser._id;
+        if (status === 200) {
+            const token = encodedToken;
+            const userId = foundUser._id;
             localStorage.setItem("token", token);
             localStorage.setItem("userId", userId);
             return { token, userId };
