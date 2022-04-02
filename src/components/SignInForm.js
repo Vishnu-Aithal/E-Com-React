9a1a2095ec7bcb910cb3fd2304d6a00cea102851
Vishnu-Aithal <90306959+Vishnu-Aithal@users.noreply.github.com>
2022-04-03 01:37:37 +0530

    fix - signinForm

--- src/components/SignInForm.js
+++ src/components/SignInForm.js
@@ -1,117 +1,103 @@
-import { signUpHandler } from "utility-functions/authHandler";
-import { useNavigate } from "react-router-dom";
+import { signInHandler } from "utility-functions/authHandler";
+import { useNavigate, useLocation, Link } from "react-router-dom";
 import { useAuth } from "contexts/auth-context";
-export const SignUpForm = () => {
-    const { authDispatch } = useAuth();
+import { useLoader } from "contexts/loader-context";
+export const SignInForm = () => {
     const navigate = useNavigate();
+    const location = useLocation();
+    const { authDispatch } = useAuth();
+    const { showLoader, hideLoader } = useLoader();
     return (
-        <form
-            onSubmit={async (e) => {
-                e.preventDefault();
-                const response = await signUpHandler(
-                    e.target.email.value,
-                    e.target.password.value,
-                    e.target.firstName.value,
-                    e.target.lastName.value
-                );
-                if (response) {
-                    authDispatch({
-                        type: "LOGIN",
-                        payload: response,
-                    });
-                    navigate("/");
-                }
-            }}
-            className="p-4 w-fit mx-auto br-2 mt-6">
-            <h2 className="heading-md text-center mb-4">Sign Up</h2>
-
-            <div className="input">
+        <>
+            <form
+                onSubmit={async (e) => {
+                    e.preventDefault();
+                    const response = await signInHandler(
+                        e.target.email.value,
+                        e.target.password.value,
+                        showLoader,
+                        hideLoader
+                    );
+                    if (response) {
+                        authDispatch({
+                            type: "LOGIN",
+                            payload: response,
+                        });
+                        navigate(
+                            location.pathname !== "/sign-in"
+                                ? location.pathname
+                                : "/"
+                        );
+                    }
+                }}
+                className="p-4 w-fit mx-auto br-2 mt-6">
+                <h2 className="heading-md text-center mb-4">Sign In</h2>
+                <div className="input">
+                    <input
+                        className="input__field"
+                        type="email"
+                        name="email"
+                        id="email"
+                        placeholder="Enter Email"
+                        required
+                    />
+                    <label className="input__float-label" htmlFor="email">
+                        Email
+                    </label>
+                    <span className="input__required-text"></span>
+                </div>
+                <div className="input">
+                    <input
+                        className="input__field"
+                        type="password"
+                        name="password"
+                        id="password"
+                        placeholder="Enter Password"
+                        minLength="8"
+                        required
+                    />
+                    <label className="input__float-label" htmlFor="password">
+                        Password
+                    </label>
+                    <span className="input__required-text"></span>
+                </div>
+                <a className="d-inline-block text-end text-sm w-100p" href="">
+                    Forgot password?
+                </a>
+                <div className="input ms-1">
+                    <input type="checkbox" className="" name="tandc" id="" />
+                    <label className="text-sm" htmlFor="">
+                        Remember Me
+                    </label>
+                </div>
                 <input
-                    type="text"
-                    className="input__field"
-                    name="firstName"
-                    placeholder="First name"
-                    required
-                />
-                <label className="input__float-label" htmlFor="name">
-                    First Name
-                </label>
-                <span className="input__required-text"></span>
-            </div>
-            <div className="input">
-                <input
-                    type="text"
-                    className="input__field"
-                    name="lastName"
-                    placeholder="Last name"
-                    required
-                />
-                <label className="input__float-label" htmlFor="name">
-                    Last Name
-                </label>
-                <span className="input__required-text"></span>
-            </div>
-            <div className="input">
-                <input
-                    className="input__field"
-                    type="email"
-                    name="email"
-                    id="email"
-                    placeholder="Enter Email"
-                    required
-                />
-                <label className="input__float-label" htmlFor="email">
-                    Email
-                </label>
-                <span className="input__required-text"></span>
-            </div>
-            <div className="input">
-                <input
-                    className="input__field"
-                    type="password"
-                    name="password"
-                    id="password"
-                    placeholder="Enter New Password"
-                    minLength="8"
-                    required
-                />
-                <label className="input__float-label" htmlFor="password">
-                    New Password
-                </label>
-                <span className="input__required-text"></span>
-            </div>
-
-            <div className="input">
-                <input
-                    className="input__field"
-                    type="password"
-                    name="password"
-                    id="password2"
-                    placeholder="Confirm Password"
-                    minLength="8"
-                    required
-                />
-                <label className="input__float-label" htmlFor="password">
-                    Confirm Password
-                </label>
-                <span className="input__required-text"></span>
-            </div>
-            <div className="input mx-auto">
-                <input
-                    type="checkbox"
-                    className=""
-                    name="tandc"
-                    id=""
-                    required
-                />
-                <label className="text-sm" htmlFor="">
-                    I accept all terms and conditions
-                </label>
-            </div>
-            <input
-                className="btn btn--primary br-1 mt-2 w-100p"
-                type="submit"
-                value="Sign Up"></input>
-        </form>
+                    className="btn btn--primary br-1 mt-2 w-100p"
+                    type="submit"
+                    value="Sign In"></input>
+                <p className="helper-text mt-3">
+                    Not a member? <Link to={"/sign-up"}>Sign Up</Link>
+                </p>
+                <button
+                    className="btn btn--primary br-1 mt-2 "
+                    onClick={async () => {
+                        const response = await signInHandler(
+                            "adarshbalika@gmail.com",
+                            "adarshbalika",
+                            showLoader,
+                            hideLoader
+                        );
+                        if (response) {
+                            authDispatch({ type: "LOGIN", payload: response });
+                            navigate(
+                                location.pathname !== "/sign-in"
+                                    ? location.pathname
+                                    : "/"
+                            );
+                        }
+                    }}>
+                    Sign In Demo
+                </button>
+            </form>
+        </>
     );
 };
