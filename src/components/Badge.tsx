699d70291d7fcb0d630e4d24a23139bcd652fcf1
Vishnu-Aithal <90306959+Vishnu-Aithal@users.noreply.github.com>
2022-06-06 15:28:47 +0530

    Add Types to Component

---
+++ src/components/Badge.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+interface BadgeProps {
+    type: "count" | "status";
+    count: number;
+    position: "top-left" | "bottom-left" | "bottom-right" | "top-right";
+    shape: "square" | "pill" | "round";
+    color:
+        | "white"
+        | "black"
+        | "primary"
+        | "primary-light"
+        | "primary-dark"
+        | "secondary"
+        | "secondary-dark"
+        | "secondary-light"
+        | "red"
+        | "blue"
+        | "yellow"
+        | "green"
+        | "light-gray"
+        | "off-white";
+}
+export const Badge: React.FC<BadgeProps> = ({
+    type,
+    count,
+    position,
+    shape,
+    color,
+}) => {
+    const positions = {
+        "top-left": "badge--top-left",
+        "bottom-left": "badge--bottom-left",
+        "bottom-right": "badge--bottom-right",
+        "top-right": "",
+    };
+    const shapes = {
+        square: "br-1",
+        pill: "br-4",
+        round: "br-r",
+    };
+    const types = {
+        count: "badge--count",
+        status: "badge--status",
+    };
+    return (
+        <span
+            className={`badge ${types[type] ?? "badge--count"} ${
+                positions[position] ?? ""
+            } ${shapes[shape] ?? "br-4"} bg-${color}`}
+            data-count={count}></span>
+    );
+};
---
+++ src/components/Card.tsx
@@ -0,0 +1,314 @@
+import { useNavigate, useLocation } from "react-router-dom";
+import { useCartWishlist } from "contexts/cart-wishlist-context";
+import { useAuth } from "contexts/auth-context";
+import { useLoader } from "contexts/loader-context";
+export const Card = ({
+    type = "home-page",
+    productData = {
+        outOfStock: false,
+        title: "title",
+        brand: "brand",
+        price: "100",
+        image: "https://picsum.photos/200/300",
+    },
+    badge = "",
+    inCart = false,
+    inWishlist = false,
+    addToWishlist = () => {},
+    moveToWishlist = () => {},
+    moveToCart = () => {},
+    addToCart = () => {},
+    removeFromCart = () => {},
+    removeFromWishlist = () => {},
+    increaseQuantity = () => {},
+    decreaseQuantity = () => {},
+    cartQuantity = 0,
+}) => {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const { cartWishlistDispatch: dispatch } = useCartWishlist();
+    const {
+        authState: { isLoggedIn, token },
+    } = useAuth();
+    const { showLoader, hideLoader } = useLoader();
+    return (
+        <div className="card shadow-sm">
+            {badge && (
+                <div className="card__badge bg-yellow p-1 br-2">{badge}</div>
+            )}
+            {type === "cart" && (
+                <div
+                    className="card__dismiss"
+                    onClick={() =>
+                        isLoggedIn
+                            ? removeFromCart(
+                                  productData,
+                                  dispatch,
+                                  token,
+                                  showLoader,
+                                  hideLoader
+                              )
+                            : navigate("/sign-in", {
+                                  state: { from: location.pathname },
+                              })
+                    }>
+                    <i className="fas fa-times clr-black"></i>
+                </div>
+            )}
+            {type === "wishlist" && (
+                <div
+                    className="card__dismiss"
+                    onClick={() =>
+                        isLoggedIn
+                            ? removeFromWishlist(
+                                  productData,
+                                  dispatch,
+                                  token,
+                                  showLoader,
+                                  hideLoader
+                              )
+                            : navigate("/sign-in", {
+                                  state: { from: location.pathname },
+                              })
+                    }>
+                    <i className="fas fa-heart clr-red"></i>
+                </div>
+            )}
+            {productData.outOfStock && (
+                <div className="card__overlay-wrapper">
+                    <p className="card__overlay-text heading-md">
+                        Out Of Stock
+                    </p>
+                </div>
+            )}
+            <div className="card__body p-2">
+                <div className="card__img-wrapper">
+                    <img
+                        className="card__img"
+                        src={productData.image}
+                        alt="product "
+                    />
+                </div>
+                {type === "home-page" ? (
+                    <div className="card__text-wrapper ps-2 text-center">
+                        <h3 className="card__heading heading-xs">
+                            {productData.title}
+                        </h3>
+                        <p className="text-semi-bold text-gray">
+                            by {productData.brand}
+                        </p>
+                    </div>
+                ) : (
+                    <div className="card__text-wrapper ps-2">
+                        <h3 className="card__heading heading-xs">
+                            {productData.title}
+                        </h3>
+                        <p className="text-semi-bold text-gray">
+                            by {productData.brand}
+                        </p>
+                        <p className="mt-3, text-bold text-md">
+                            {productData.outOfStock ? (
+                                <>
+                                    Price: {productData.price}{" "}
+                                    <span className="clr-red">
+                                        Out Of Stock!
+                                    </span>
+                                </>
+                            ) : (
+                                `Price: ${productData.price}`
+                            )}
+                        </p>
+                    </div>
+                )}
+            </div>
+
+            {type === "listing" && (
+                <div className="card__cta-wrapper p-2">
+                    {inCart ? (
+                        <button
+                            className="btn btn--outline-primary w-100p"
+                            onClick={() => navigate("/cart")}>
+                            Go to Cart
+                        </button>
+                    ) : (
+                        <>
+                            {inWishlist ? (
+                                <>
+                                    <button
+                                        className="btn btn--secondary w-100p"
+                                        onClick={() =>
+                                            isLoggedIn
+                                                ? moveToCart(
+                                                      productData,
+                                                      dispatch,
+                                                      token,
+                                                      showLoader,
+                                                      hideLoader
+                                                  )
+                                                : navigate("/sign-in", {
+                                                      state: {
+                                                          from: location.pathname,
+                                                      },
+                                                  })
+                                        }>
+                                        Move to Cart
+                                    </button>
+                                    <button
+                                        className="btn btn--icon clr-red ms-1"
+                                        onClick={() =>
+                                            isLoggedIn
+                                                ? removeFromWishlist(
+                                                      productData,
+                                                      dispatch,
+                                                      token,
+                                                      showLoader,
+                                                      hideLoader
+                                                  )
+                                                : navigate("/sign-in", {
+                                                      state: {
+                                                          from: location.pathname,
+                                                      },
+                                                  })
+                                        }>
+                                        <i className="fas fa-heart"></i>
+                                    </button>
+                                </>
+                            ) : (
+                                <>
+                                    <button
+                                        className="btn btn--primary w-100p"
+                                        onClick={() =>
+                                            isLoggedIn
+                                                ? addToCart(
+                                                      productData,
+                                                      dispatch,
+                                                      token,
+                                                      showLoader,
+                                                      hideLoader
+                                                  )
+                                                : navigate("/sign-in", {
+                                                      state: {
+                                                          from: location.pathname,
+                                                      },
+                                                  })
+                                        }>
+                                        Add to Cart
+                                    </button>
+                                    <button
+                                        className="btn btn--icon clr-red ms-1"
+                                        onClick={() =>
+                                            isLoggedIn
+                                                ? addToWishlist(
+                                                      productData,
+                                                      dispatch,
+                                                      token,
+                                                      showLoader,
+                                                      hideLoader
+                                                  )
+                                                : navigate("/sign-in", {
+                                                      state: {
+                                                          from: location.pathname,
+                                                      },
+                                                  })
+                                        }>
+                                        <i className="far fa-heart"></i>
+                                    </button>
+                                </>
+                            )}
+                        </>
+                    )}
+                </div>
+            )}
+
+            {type === "wishlist" && (
+                <div className="card__cta-wrapper p-2">
+                    <button
+                        className="btn btn--secondary mx-1 w-100p"
+                        onClick={() =>
+                            isLoggedIn
+                                ? moveToCart(
+                                      productData,
+                                      dispatch,
+                                      token,
+                                      showLoader,
+                                      hideLoader
+                                  )
+                                : navigate("/sign-in", {
+                                      state: { from: location.pathname },
+                                  })
+                        }>
+                        Move to Cart
+                    </button>
+                </div>
+            )}
+            {type === "home-page" && (
+                <div className="card__cta-wrapper p-2">
+                    <button
+                        className="btn btn--secondary w-100p"
+                        onClick={() => navigate("/products")}>
+                        Shop Now
+                    </button>
+                </div>
+            )}
+            {type === "cart" && (
+                <div className="card__cta-wrapper p-2">
+                    <button
+                        className="btn btn--outline-secondary mx-1"
+                        onClick={() =>
+                            isLoggedIn
+                                ? moveToWishlist(
+                                      productData,
+                                      dispatch,
+                                      token,
+                                      showLoader,
+                                      hideLoader
+                                  )
+                                : navigate("/sign-in", {
+                                      state: { from: location.pathname },
+                                  })
+                        }>
+                        Move to Wishlist
+                    </button>
+                    <div className="card__quantity ms-auto">
+                        <button
+                            className="btn btn--icon"
+                            disabled={cartQuantity <= 1}
+                            onClick={() =>
+                                isLoggedIn
+                                    ? decreaseQuantity(
+                                          productData,
+                                          dispatch,
+                                          token,
+                                          showLoader,
+                                          hideLoader
+                                      )
+                                    : navigate("/sign-in", {
+                                          state: { from: location.pathname },
+                                      })
+                            }>
+                            -
+                        </button>
+                        <p className="card__item-count">{cartQuantity}</p>
+                        <button
+                            className="btn btn--icon"
+                            onClick={() =>
+                                isLoggedIn
+                                    ? increaseQuantity(
+                                          productData,
+                                          dispatch,
+                                          token,
+                                          showLoader,
+                                          hideLoader
+                                      )
+                                    : navigate("/sign-in", {
+                                          state: { from: location.pathname },
+                                      })
+                            }>
+                            +
+                        </button>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
