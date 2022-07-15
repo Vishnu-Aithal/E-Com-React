79cd25c8c33ae000f7be078755bf14ef2fcda6b5
Vishnu-Aithal <90306959+Vishnu-Aithal@users.noreply.github.com>
2022-07-15 14:29:31 +0530

    feat - sort orders by latest

--- src/routes/OrdersPage/OrdersPage.tsx
+++ src/routes/OrdersPage/OrdersPage.tsx
@@ -1,15 +1,26 @@
 import { OrderDisplay } from "components/Order/OrderDisplay";
 import { useUser } from "contexts/user-context";
+import { useEffect, useState } from "react";
+import { Order } from "types/Order";
 import classes from "./OrdersPage.module.css";
 
 export const OrdersPage: React.FC = () => {
     const {
         userState: { orders },
     } = useUser();
+    const [sortedOrders, setSortedOrders] = useState<Order[]>([]);
+    useEffect(() => {
+        let sorted = [...orders];
+        sorted.sort(
+            (a, b) =>
+                Date.parse(b.createdAt || "") - Date.parse(a.createdAt || "")
+        );
+        setSortedOrders(sorted);
+    }, [orders]);
     return (
         <div className={classes["container"]}>
-            {orders.length === 0 && <h3>No Orders Placed</h3>}
-            {orders.map((order) => (
+            {sortedOrders.length === 0 && <h3>No Orders Placed</h3>}
+            {sortedOrders.map((order) => (
                 <OrderDisplay order={order} key={order._id} />
             ))}
         </div>
