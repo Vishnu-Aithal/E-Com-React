c84b15e6fd03785499a63d3e4d350e06d638a767
Vishnu-Aithal <90306959+Vishnu-Aithal@users.noreply.github.com>
2022-07-15 14:26:10 +0530

    style - re - style filter

--- src/components/Filter/FilterOptions.tsx
+++ src/components/Filter/FilterOptions.tsx
@@ -1,9 +1,10 @@
 import { useFilter } from "contexts/filter-context";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Product } from "types/Product";
 import classes from "./FilterOptions.module.css";
 
 export const FilterOptions: React.FC = () => {
+    const [showFilter, setShowFilter] = useState(false);
     const ratings = [4, 3, 2, 1];
     const categories = ["Sports", "Casual", "Formal"];
     const brands = ["Hike", "Adibas", "WoodSky"];
@@ -36,191 +37,206 @@ export const FilterOptions: React.FC = () => {
     }, [dispatch]);
     return (
         <aside className={classes["filter"]}>
-            <div className={`${classes["filter__header"]}  m-2`}>
+            <div
+                className={`${classes["filter__header"]}  m-2`}
+                onClick={() => setShowFilter((prev) => !prev)}>
                 <h4 className="heading-xs text-bold">Filters</h4>
+                <button className={`btn ${classes["filter__toggle"]}`}>
+                    {showFilter ? "Hide" : "Show"}
+                </button>
                 <p
-                    className={`${classes["filter__clear"]} ms-auto`}
-                    onClick={() => dispatch({ type: "CLEAR_FILTERS" })}>
+                    className={classes["filter__clear"]}
+                    onClick={(e) => {
+                        e.stopPropagation();
+                        dispatch({ type: "CLEAR_FILTERS" });
+                    }}>
                     Clear
                 </p>
             </div>
-            <div className={`${classes["filter__price"]} m-2`}>
-                <h4 className="heading-xs text-bold">Price</h4>
-                <input
-                    className={classes["filter__price-slider"]}
-                    type="range"
-                    min={minPrice}
-                    max={maxPrice}
-                    list="price-steps"
-                    value={filters.priceRange || maxPrice}
-                    onChange={(e) =>
-                        dispatch({
-                            type: "SET_PRICE_RANGE",
-                            payload: e.target.value,
-                        })
-                    }
-                />
-                <datalist id="price-steps" className={classes["price-steps"]}>
-                    <option
-                        value={minPrice}
-                        label={minPrice.toString()}></option>
-                    <div className="text-bold text-md">
-                        {filters.priceRange ?? maxPrice}
-                    </div>
-                    <option
-                        value={maxPrice}
-                        label={maxPrice.toString()}></option>
-                </datalist>
-            </div>
-            <div className={`${classes["filter__category"]} m-2`}>
-                <h4 className="heading-xs text-bold">Category</h4>
-                {categories.map((category) => (
-                    <label key={category}>
-                        <input
-                            type="checkbox"
-                            onChange={() =>
-                                dispatch({
-                                    type: "CATEGORY",
-                                    payload: category,
-                                })
-                            }
-                            checked={filters.category.includes(category)}
-                        />{" "}
-                        {category}
-                    </label>
-                ))}
-            </div>
-            <div className={`${classes["filter__category"]} m-2`}>
-                <h4 className="heading-xs text-bold">Brand</h4>
-                {brands.map((brand) => (
-                    <label key={brand}>
-                        <input
-                            type="checkbox"
-                            onChange={() =>
-                                dispatch({
-                                    type: "BRAND",
-                                    payload: brand,
-                                })
-                            }
-                            checked={filters.brand.includes(brand)}
-                        />{" "}
-                        {brand}
-                    </label>
-                ))}
-            </div>
-            <div className={`${classes["filter__category"]} m-2`}>
-                <h4 className="heading-xs text-bold">Type</h4>
-                {types.map((type) => (
-                    <label key={type}>
-                        <input
-                            type="checkbox"
-                            onChange={() =>
-                                dispatch({
-                                    type: "TYPE",
-                                    payload: type,
-                                })
-                            }
-                            checked={filters.type.includes(type)}
-                        />{" "}
-                        {type}
-                    </label>
-                ))}
-            </div>
-            <div className={`${classes["filter__rating"]} m-2`}>
-                <h4 className="heading-xs text-bold">Rating</h4>
-                {ratings.map((rating) => (
-                    <label key={rating}>
+            <div
+                className={`${classes["filter-body"]} ${
+                    showFilter ? classes["show-filter"] : ""
+                }`}>
+                <div className={`${classes["filter__price"]} m-2`}>
+                    <h4 className="heading-xs text-bold">Price</h4>
+                    <input
+                        className={classes["filter__price-slider"]}
+                        type="range"
+                        min={minPrice}
+                        max={maxPrice}
+                        list="price-steps"
+                        value={filters.priceRange || maxPrice}
+                        onChange={(e) =>
+                            dispatch({
+                                type: "SET_PRICE_RANGE",
+                                payload: e.target.value,
+                            })
+                        }
+                    />
+                    <datalist
+                        id="price-steps"
+                        className={classes["price-steps"]}>
+                        <option
+                            value={minPrice}
+                            label={minPrice.toString()}></option>
+                        <div className="text-bold text-md">
+                            {filters.priceRange ?? maxPrice}
+                        </div>
+                        <option
+                            value={maxPrice}
+                            label={maxPrice.toString()}></option>
+                    </datalist>
+                </div>
+                <div className={`${classes["filter__category"]} m-2`}>
+                    <h4 className="heading-xs text-bold">Category</h4>
+                    {categories.map((category) => (
+                        <label key={category}>
+                            <input
+                                type="checkbox"
+                                onChange={() =>
+                                    dispatch({
+                                        type: "CATEGORY",
+                                        payload: category,
+                                    })
+                                }
+                                checked={filters.category.includes(category)}
+                            />{" "}
+                            {category}
+                        </label>
+                    ))}
+                </div>
+                <div className={`${classes["filter__category"]} m-2`}>
+                    <h4 className="heading-xs text-bold">Brand</h4>
+                    {brands.map((brand) => (
+                        <label key={brand}>
+                            <input
+                                type="checkbox"
+                                onChange={() =>
+                                    dispatch({
+                                        type: "BRAND",
+                                        payload: brand,
+                                    })
+                                }
+                                checked={filters.brand.includes(brand)}
+                            />{" "}
+                            {brand}
+                        </label>
+                    ))}
+                </div>
+                <div className={`${classes["filter__category"]} m-2`}>
+                    <h4 className="heading-xs text-bold">Type</h4>
+                    {types.map((type) => (
+                        <label key={type}>
+                            <input
+                                type="checkbox"
+                                onChange={() =>
+                                    dispatch({
+                                        type: "TYPE",
+                                        payload: type,
+                                    })
+                                }
+                                checked={filters.type.includes(type)}
+                            />{" "}
+                            {type}
+                        </label>
+                    ))}
+                </div>
+                <div className={`${classes["filter__rating"]} m-2`}>
+                    <h4 className="heading-xs text-bold">Rating</h4>
+                    {ratings.map((rating) => (
+                        <label key={rating}>
+                            <input
+                                type="radio"
+                                name="filter-rating"
+                                checked={filters.rating === rating}
+                                onChange={() =>
+                                    dispatch({
+                                        type: "SET_RATING",
+                                        payload: rating,
+                                    })
+                                }
+                            />{" "}
+                            {rating} Stars & Above
+                        </label>
+                    ))}
+                </div>
+                <div className={`${classes["filter__sort"]} m-2`}>
+                    <h4 className="heading-xs text-bold">Sort By Price</h4>
+                    <label>
                         <input
                             type="radio"
-                            name="filter-rating"
-                            checked={filters.rating === rating}
+                            name="price-sort"
+                            checked={sorters.byPrice === "low-to-high"}
                             onChange={() =>
                                 dispatch({
-                                    type: "SET_RATING",
-                                    payload: rating,
+                                    type: "SORT_BY_PRICE",
+                                    payload: "low-to-high",
                                 })
                             }
                         />{" "}
-                        {rating} Stars & Above
+                        Price - Low to High
                     </label>
-                ))}
-            </div>
-            <div className={`${classes["filter__sort"]} m-2`}>
-                <h4 className="heading-xs text-bold">Sort By Price</h4>
-                <label>
-                    <input
-                        type="radio"
-                        name="price-sort"
-                        checked={sorters.byPrice === "low-to-high"}
-                        onChange={() =>
-                            dispatch({
-                                type: "SORT_BY_PRICE",
-                                payload: "low-to-high",
-                            })
-                        }
-                    />{" "}
-                    Price - Low to High
-                </label>
-                <label>
-                    <input
-                        type="radio"
-                        name="price-sort"
-                        checked={sorters.byPrice === "high-to-low"}
-                        onChange={() =>
-                            dispatch({
-                                type: "SORT_BY_PRICE",
-                                payload: "high-to-low",
-                            })
-                        }
-                    />{" "}
-                    Price - High to Low
-                </label>
-            </div>
-            <div className={`${classes["filter__sort"]} m-2`}>
-                <h4 className="heading-xs text-bold">Sort By Rating</h4>
-                <label>
-                    <input
-                        type="radio"
-                        name="rating-sort"
-                        checked={sorters.byRating === "low-to-high"}
-                        onChange={() =>
-                            dispatch({
-                                type: "SORT_BY_RATING",
-                                payload: "low-to-high",
-                            })
-                        }
-                    />{" "}
-                    Rating - Low to High
-                </label>
-                <label>
-                    <input
-                        type="radio"
-                        name="rating-sort"
-                        checked={sorters.byRating === "high-to-low"}
-                        onChange={() =>
-                            dispatch({
-                                type: "SORT_BY_RATING",
-                                payload: "high-to-low",
-                            })
-                        }
-                    />{" "}
-                    Rating - High to Low
-                </label>
-            </div>
-            <div className={`${classes["filter__category"]} m-2`}>
-                <h4 className="heading-xs text-bold">Other</h4>
-                <label>
-                    <input
-                        type="checkbox"
-                        onChange={() =>
-                            dispatch({
-                                type: "SHOW_OUT_OF_STOCK",
-                            })
-                        }
-                        checked={filters.showOutOfStock}
-                    />{" "}
-                    Show Out Of Stock
-                </label>
+                    <label>
+                        <input
+                            type="radio"
+                            name="price-sort"
+                            checked={sorters.byPrice === "high-to-low"}
+                            onChange={() =>
+                                dispatch({
+                                    type: "SORT_BY_PRICE",
+                                    payload: "high-to-low",
+                                })
+                            }
+                        />{" "}
+                        Price - High to Low
+                    </label>
+                </div>
+                <div className={`${classes["filter__sort"]} m-2`}>
+                    <h4 className="heading-xs text-bold">Sort By Rating</h4>
+                    <label>
+                        <input
+                            type="radio"
+                            name="rating-sort"
+                            checked={sorters.byRating === "low-to-high"}
+                            onChange={() =>
+                                dispatch({
+                                    type: "SORT_BY_RATING",
+                                    payload: "low-to-high",
+                                })
+                            }
+                        />{" "}
+                        Rating - Low to High
+                    </label>
+                    <label>
+                        <input
+                            type="radio"
+                            name="rating-sort"
+                            checked={sorters.byRating === "high-to-low"}
+                            onChange={() =>
+                                dispatch({
+                                    type: "SORT_BY_RATING",
+                                    payload: "high-to-low",
+                                })
+                            }
+                        />{" "}
+                        Rating - High to Low
+                    </label>
+                </div>
+                <div className={`${classes["filter__category"]} m-2`}>
+                    <h4 className="heading-xs text-bold">Other</h4>
+                    <label>
+                        <input
+                            type="checkbox"
+                            onChange={() =>
+                                dispatch({
+                                    type: "SHOW_OUT_OF_STOCK",
+                                })
+                            }
+                            checked={filters.showOutOfStock}
+                        />{" "}
+                        Show Out Of Stock
+                    </label>
+                </div>
             </div>
         </aside>
     );
